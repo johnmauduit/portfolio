67bf66d9238db3da321e14911e383e5514263cd2
john <johnmauduit@gmail.com>
2018-11-04 23:05:38 +0100

    Add session storage for multilingual and update json file

--- dist/js/lang.js
+++ dist/js/lang.js
@@ -1,46 +1,48 @@
+/**
+ * @Author John Mauduit
+ *
+ * Jquery language functions
+ * Listen on click, on the flag button in index.thml
+ * Read entry from json file
+ * Set items to local storage
+ * Get items fron local storage
+ */
+
 //start point
 $(document).ready(function() {
-  // get lang from click
   let jsonData = {};
-  //retrieve localStorage if there is one
   let ls = false;
   jsonData.href = window.location.href;
-  //console.log(jsonData);
+
   function getLs() {
-    if (window.localStorage["local"]) {
-      ls = JSON.parse(window.localStorage.getItem("local"));
+    if (window.sessionStorage["local"]) {
+      ls = JSON.parse(window.sessionStorage.getItem("local"));
       if (ls) {
         $.each(ls, (key, val) => {
           $("#" + key).text(val);
-          //console.log(key + " : " + val);
         });
       }
     }
   }
   getLs();
+
+  // get language from flag click
   $("[data-lang]").click(function(e) {
     e.preventDefault();
+
     let lang = $(this).attr("data-lang");
     let jsonFile = `lang/${lang}.json`;
 
-    // get key:value from json file
+    // get key:value from json files
     $.getJSON(jsonFile, data => {
       $.each(data, (k, v) => {
         // set json file to the localStorage
         if ("localStorage" in window) {
           let jsonData = JSON.stringify(data);
-          window.localStorage.setItem("local", jsonData);
-          //console.log(window.localStorage);
-          //console.log(localStorage.getItem("local"));
-          //console.log(data);
-          //console.log(jsonData);
-          //localStorage.getItem("local");
-          //console.log(k, ": " + v);
+          window.sessionStorage.setItem("local", jsonData);
           $("#" + k).text(v);
         }
       });
-      ls = JSON.parse(window.localStorage.getItem("local"));
     });
-    //console.log(lang);
   });
 });
